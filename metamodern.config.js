d54d0f6d5082cffeeb156b5487d3529d74aa56b1
Daniel Narey <danielnarey@users.noreply.github.com>
2020-03-06 20:25:43 -0500

    try again

--- metamodern.config.js
+++ metamodern.config.js
@@ -31,6 +31,24 @@ const postcss = {
 };
 
 
+const lsh = (options = {}) => (l = 62) => (s = 0) => (h) => {
+  if (!h && h !== 0) {
+    return `hsl(0,0%,${l}%)`;
+  }
+
+  const namedHue = named(options).get(h);
+
+  if (!namedHue && namedHue !== 0) {
+    return `hsl(${h},${s}%,${l}%)`;
+  }
+
+  return `hsl(${namedHue},${s}%,${l}%)`;
+};
+
+
+const _lsh = lsh({});
+
+
 const bgPalette = new Map([
   ['lighter', _lsh(92)(25)],
   ['light', _lsh(87)(40)],
