1ae03ca7320dc7827bf013f3bb3b52b734a37323
Daniel Narey <danielnarey@users.noreply.github.com>
2020-03-11 12:22:03 -0400

    update design system

--- metamodern.config.js
+++ metamodern.config.js
@@ -17,26 +17,7 @@ const postcss = {
 };
 
 
-const { lsh } = colorSystem();
-const _lsh = lsh;
-
-const bgPalette = new Map([
-  ['lighter', _lsh(92)(25)],
-  ['light', _lsh(87)(40)],
-  ['medium-light', _lsh(77)(55)],
-  ['bright', _lsh(62)(85)],
-  ['soft', _lsh(62)(55)],
-  ['softer', _lsh(57)(40)],
-  ['medium', _lsh(47)(70)],
-  ['medium-dark', _lsh(32)(55)],
-  ['dark', _lsh(22)(40)],
-  ['darker', _lsh(17)(40)],
-]);
-
-
 const designSystemConfig = {
-  bgPalette,
-  textPalette: bgPalette,
   variants: {
     borderWidth: ['responsive'],
     borderRadius: ['responsive'],
